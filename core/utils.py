84d5bfba5ca8e92e4aea2915070203ddb885b760
jiggy <jiggyjiggy0323@gmail.com>
2022-05-19 15:35:59 +0900

    feat: Create identification decorator

--- core/utils.py
+++ core/utils.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 
 from users.models import User
 
+
 def login_required(func):
     def wrapper(self, request, *args, **kwargs):
         try:
@@ -22,4 +23,31 @@ def login_required(func):
             return JsonResponse({'MESSAGE':'INVALID_USER'}, status=400)
         
         return func(self, request, *args, **kwargs)
+    return wrapper
+
+
+def identification_decorator(func):
+    def wrapper(self, request, *args, **kwrags):
+        try:
+            request.user = None
+
+            token = request.headers.get('Authorization', None)
+            if token:
+                payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+                request.user = User.objects.get(id=payload['id'])
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'MESSAGE': 'INVALID_TOKEN'}, status=401)
+
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({'MESSAGE': 'EXPIRED_TOKEN'}, status=401)
+
+        except jwt.InvalidSignatureError:
+            return JsonResponse({'MESSAGE': 'INVALID_SIGNATURE'}, status=401)
+
+        return func(self, request, *args, **kwrags)
+
     return wrapper
